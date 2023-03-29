8719ef829a71e791255392b4bbaacf97211bb079
Ryan Dick <ryanjdick3@gmail.com>
2023-03-29 08:41:18 -0400

    Improve documentation of image processing lambda. And, add sharp image rotate() transformation.

--- functions/image-processing/index.js
+++ functions/image-processing/index.js
@@ -7,56 +7,79 @@ const Sharp = require('sharp');
 
 const S3 = new AWS.S3({ signatureVersion: 'v4', httpOptions: { agent: new https.Agent({ keepAlive: true }) } });
 const S3_ORIGINAL_IMAGE_BUCKET = process.env.originalImageBucketName;
-const S3_TRANSFORMED_IMAGE_BUCKET = process.env.transformedImageBucketName;
-const TRANSFORMED_IMAGE_CACHE_TTL = process.env.transformedImageCacheTTL;
+const S3_TRANSFORMED_IMAGE_BUCKET = process.env.sharpImageBucketName;
+const TRANSFORMED_IMAGE_CACHE_TTL = process.env.sharpImageCacheTTL;
 const SECRET_KEY = process.env.secretKey;
 const LOG_TIMING = process.env.logTiming;
 
 exports.handler = async (event) => {
-    // First validate if the request is coming from CloudFront
-    if (!event.headers['x-origin-secret-header'] || !(event.headers['x-origin-secret-header'] === SECRET_KEY)) return sendError(403, 'Request unauthorized', event);
-    // Validate if this is a GET request
-    if (!event.requestContext || !event.requestContext.http || !(event.requestContext.http.method === 'GET')) return sendError(400, 'Only GET method is supported', event);
-    // An example of expected path is /rio/images/1.jpg/format=auto,width=100 or /rio/images/1.jpg/original where /rio/images/1.jpg is the path of the original image
+    // Validate that the request is coming from CloudFront.
+    if (
+        !event.headers['x-origin-secret-header'] ||
+        !(event.headers['x-origin-secret-header'] === SECRET_KEY)
+    ) {
+        return sendError(403, 'Request unauthorized.', event);
+    }
+
+    // Validate that it is a GET request.
+    if (
+        !event.requestContext ||
+        !event.requestContext.http ||
+        !(event.requestContext.http.method === 'GET')
+    ) {
+        return sendError(400, 'Only GET method is supported.', event);
+    }
+
+    // Examples  of expected http paths:
+    //   - /rio/images/1.jpg/format=auto,width=100
+    //   - /rio/images/1.jpg/original
     var imagePathArray = event.requestContext.http.path.split('/');
-    // get the requested image operations
+    // Get the requested image operations. (e.g. "format=auto,width=100")
     var operationsPrefix = imagePathArray.pop();
-    // get the original image path images/rio/1.jpg
+    // Get the original image path. (e.g. images/rio/1.jpg)
     imagePathArray.shift();
     var originalImagePath = imagePathArray.join('/');
-    // timing variable
+
+    // Timing variables.
     var timingLog = "perf ";
     var startTime = performance.now();
-    // Downloading original image
+
+    // Download original image.
     let originalImage;
     let contentType;
     try {
         originalImage = await S3.getObject({ Bucket: S3_ORIGINAL_IMAGE_BUCKET, Key: originalImagePath }).promise();
         contentType = originalImage.ContentType;
     } catch (error) {
-        return sendError(500, 'error downloading original image', error);
+        return sendError(500, 'Error downloading original image.', error);
     }
-    let sharpObject = Sharp(originalImage.Body);
-    let transformedImage;
-    //  execute the requested operations 
-    var operationsJSON = {};
-    var operationsArray = operationsPrefix.split(',');
+
+    // Split operationsPrefix into individual fields.
+    let operations = {};
+    let operationsArray = operationsPrefix.split(',');
     operationsArray.forEach(operation => {
-        var operationKV = operation.split("=");
-        operationsJSON[operationKV[0]] = operationKV[1];
+        let operationKV = operation.split("=");
+        operations[operationKV[0]] = operationKV[1];
     });
+
+    // Perform the requested image transformation.
+    let sharpImage = Sharp(originalImage.Body);
     timingLog = timingLog + parseInt(performance.now() - startTime) + ' ';
     startTime = performance.now();
     try {
-        // check if resizing is requested
+        // Keep the correct orientation while still discarding all other EXIF data.
+        sharpImage = sharpImage.rotate();
+
+        // Apply resizing if requested.
         var resizingOptions = {};
-        if (operationsJSON['width']) resizingOptions.width = parseInt(operationsJSON['width']);
-        if (operationsJSON['height']) resizingOptions.height = parseInt(operationsJSON['height']);
-        if (resizingOptions) transformedImage = sharpObject.resize(resizingOptions);
-        // check if formatting is requested
-        if (operationsJSON['format']) {
+        if (operations['width']) resizingOptions.width = parseInt(operations['width']);
+        if (operations['height']) resizingOptions.height = parseInt(operations['height']);
+        if (resizingOptions) sharpImage = sharpImage.resize(resizingOptions);
+
+        // Apply formatting if requested.
+        if (operations['format']) {
             var isLossy = false;
-            switch (operationsJSON['format']) {
+            switch (operations['format']) {
                 case 'jpeg': contentType = 'image/jpeg'; isLossy = true; break;
                 case 'svg': contentType = 'image/svg+xml'; break;
                 case 'gif': contentType = 'image/gif'; break;
@@ -65,23 +88,25 @@ exports.handler = async (event) => {
                 case 'avif': contentType = 'image/avif'; isLossy = true; break;
                 default: contentType = 'image/jpeg'; isLossy = true;
             }
-            if (operationsJSON['quality'] && isLossy) {
-                transformedImage = transformedImage.toFormat(operationsJSON['format'], {
-                    quality: parseInt(operationsJSON['quality']),
+            if (operations['quality'] && isLossy) {
+                sharpImage = sharpImage.toFormat(operations['format'], {
+                    quality: parseInt(operations['quality']),
                 });
-            } else transformedImage = transformedImage.toFormat(operationsJSON['format']);
+            } else sharpImage = sharpImage.toFormat(operations['format']);
         }
-        transformedImage = await transformedImage.toBuffer();
+        sharpImage = await sharpImage.toBuffer();
     } catch (error) {
         return sendError(500, 'error transforming image', error);
     }
+
     timingLog = timingLog + parseInt(performance.now() - startTime) + ' ';
     startTime = performance.now();
-    // upload transformed image back to S3 if required in the architecture
+
+    // Upload transformed image back to S3 if this feature is enabled.
     if (S3_TRANSFORMED_IMAGE_BUCKET) {
         try {
             await S3.putObject({
-                Body: transformedImage,
+                Body: sharpImage,
                 Bucket: S3_TRANSFORMED_IMAGE_BUCKET,
                 Key: originalImagePath + '/' + operationsPrefix,
                 ContentType: contentType,
@@ -93,12 +118,14 @@ exports.handler = async (event) => {
             sendError(500, 'Could not upload transformed image to S3.', error);
         }
     }
+
     timingLog = timingLog + parseInt(performance.now() - startTime) + ' ';
     if (LOG_TIMING === 'true') console.log(timingLog);
-    // return transformed image
+
+    // Return the transformed image.
     return {
         statusCode: 200,
-        body: transformedImage.toString('base64'),
+        body: sharpImage.toString('base64'),
         isBase64Encoded: true,
         headers: {
             'Content-Type': contentType,
